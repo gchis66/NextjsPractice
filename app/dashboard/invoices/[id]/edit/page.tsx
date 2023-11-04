8acb7efee9b537aa26a046d4e7a5c702ddbe6b95
gchis66 <gchis66@gmail.com>
2023-11-03 23:39:16 -0400

    updates up to lesson 15

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -1,6 +1,7 @@
 import Form from "@/app/ui/invoices/edit-form";
 import Breadcrumbs from "@/app/ui/invoices/breadcrumbs";
 import { fetchInvoiceById, fetchCustomers } from "@/app/lib/data";
+import { notFound } from "next/navigation";
 
 export default async function Page({ params }: { params: { id: string } }) {
   const id = params.id;
@@ -20,11 +21,7 @@ export default async function Page({ params }: { params: { id: string } }) {
           },
         ]}
       />
-      {invoice ? (
-        <Form invoice={invoice} customers={customers} />
-      ) : (
-        <p>Invoice not found</p>
-      )}
+      {invoice ? <Form invoice={invoice} customers={customers} /> : notFound()}
     </main>
   );
 }
